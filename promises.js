5d71540c145e9f15bac9c4f7e536a18d087e7bf1
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-02-04 11:24:39 +0530

    promise.all

--- promises.js
+++ promises.js
@@ -6,34 +6,45 @@ const posts = [
 ];
 
 
-function getPosts(){
-    // getting data from server has some delay
-    setTimeout(() => {
-        let output = '';
-        posts.forEach((post, index) =>
-        {
-            output += `<li>${post.title}</li>`;
-        });
-        document.body.innerHTML = output;
-    }, 1000);
-}
+// function getPosts(){
+//     // getting data from server has some delay
+//     setTimeout(() => {
+//         let output = '';
+//         posts.forEach((post, index) =>
+//         {
+//             output += `<li>${post.title}</li>`;
+//         });
+//         document.body.innerHTML = output;
+//     }, 1000);
+// }
 
-function createPost(post){
-    return new Promise((resolve, reject) => { // Promises takes in 2 callbacks as parameter
-        setTimeout(() => {
-        posts.push(post);
-        let err = false;
+// function createPost(post){
+//     return new Promise((resolve, reject) => { // Promises takes in 2 callbacks as parameter
+//         setTimeout(() => {
+//         posts.push(post);
+//         let err = false;
         
-        if(!err){
-            resolve();
-        }
-        else{
-            reject('Error: Something went wrong!!!');
-        }
-    }, 2000);
-    });
-}
+//         if(!err){
+//             resolve();
+//         }
+//         else{
+//             reject('Error: Something went wrong!!!');
+//         }
+//     }, 2000);
+//     });
+// }
 
-createPost({'title': 'Post 3', 'body': 'This is the third post'})
-.then(getPosts) // executes resolve
-.catch(err => console.log(err)); // executes reject
+// createPost({'title': 'Post 3', 'body': 'This is the third post'})
+// .then(getPosts) // executes resolve
+// .catch(err => console.log(err)); // executes reject
+
+// Promise.all
+const promise1 = Promise.resolve('Hello world');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => 
+    setTimeout(resolve, 1000, 'Goodbye')
+);
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()); 
+// fetch api requires 2 then(), hence we are giving one here and one below where it is called
+
+Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values));
